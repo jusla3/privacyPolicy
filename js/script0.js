f46be2a13e857f3d9c937946f3fe1626a1ef84aa
jusla3 <jusla3@yahoo.co.uk>
2018-04-26 17:28:52 +0100

    Uploaded the files
    from Hail windows Computer

---
+++ js/script0.js
@@ -0,0 +1,24 @@
+function myFunction2() {
+var $pop = $('#popUp'),
+		  $agree = $('#agree');
+
+$pop.show();
+
+    $("#popUp").css("margin-left", "0px");
+    $("#plus").css("margin-left", "-625px");
+
+
+  $("#close").click(function() {
+    $("#popUp").css("margin-left", "-625px");
+    $("#plus").css("margin-left", "0px");
+
+  });
+
+}
+$(document).scroll(function() {
+  var scroll = $(this).scrollTop();
+  if (scroll >= 150) {
+    $("#popUp").css("margin-left", "-505px");
+    $("#plus").css("margin-left", "0px");
+}
+});
