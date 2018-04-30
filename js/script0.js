e56e8a0f8ee1bbdd1e588271b5ff836880446b4a
hai-lazaro <lazaro@hailltd.org>
2018-04-30 12:42:11 +0100

    script0.js When you click the agreed button the Policy statment box just dissappears fast. Not the expected result :: We want the statment popup window to gradually disappear.

--- js/script0.js
+++ js/script0.js
@@ -13,8 +13,14 @@ $pop.show();
     $("#plus").css("margin-left", "0px");
 
   });
+	$("#submitB").click(function() {
+		$("#popUp").css("margin-left", "-625px");
+		$("#plus").css("margin-left", "0px");
+
+	});
 
 }
+
 $(document).scroll(function() {
   var scroll = $(this).scrollTop();
   if (scroll >= 150) {
