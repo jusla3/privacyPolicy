0b0ad615e32c9176e2136db26b2dbe4d8c127f3e
hai-lazaro <lazaro@hailltd.org>
2018-04-30 16:04:12 +0100

    Stored checked value after reload not working
    https://www.sitepoint.com/quick-tip-persist-checkbox-checked-state-after-page-reload/

--- js/script0.js
+++ js/script0.js
@@ -1,10 +1,26 @@
+var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
+    $checkboxes = $("#checkbox-container :checkbox");
+
+$checkboxes.on("change", function(){
+  $checkboxes.each(function(){
+    checkboxValues[this.id] = this.checked;
+  });
+
+  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+});
+
+// On page load
+$.each(checkboxValues, function(key, value) {
+  $("#" + key).prop('checked', value);
+
+});
+
 function myFunction2() {
 var $pop = $('#popUp'),
 		  $agree = $('#agree');
 
 $pop.show();
-
-    $("#popUp").css("margin-left", "0px");
+$("#popUp").css("margin-left", "0px");
     $("#plus").css("margin-left", "-625px");
 
 
@@ -13,18 +29,19 @@ $pop.show();
     $("#plus").css("margin-left", "0px");
 
   });
-	$("#submitB").click(function() {
-		$("#popUp").css("margin-left", "-625px");
-		$("#plus").css("margin-left", "0px");
 
-	});
 
-}
+	$(document).ready(function() {
+  $("#notification-dropdown").hover(function(e) {
+    var $btn = $('button', this);
+    if (!$btn.hasClass('clicked')) {
+      $btn.css("backgroundColor", e.type == 'mouseenter' ? "#fff" : '#9d9d9d');
+    }
+  }).click(function() {
+    $('button', this).addClass('clicked').css("backgroundColor", 'red');
 
-$(document).scroll(function() {
-  var scroll = $(this).scrollTop();
-  if (scroll >= 150) {
-    $("#popUp").css("margin-left", "-505px");
-    $("#plus").css("margin-left", "0px");
-}
+  })
+
+
 });
+};
