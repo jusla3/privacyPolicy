3dd0060bc56af93b0ec7fc0e2fd42da47a81fde0
hai-lazaro <lazaro@hailltd.org>
2018-05-08 17:21:02 +0100

    major changes but not working

--- js/script0.js
+++ js/script0.js
@@ -1,47 +1,9 @@
 
-//==================================================
-var t = 0;
 
-var $pop = $('#popUp');
-
-$('#C4').click(function(){
-  var errr = $(this);
-  console.log("Which Button ",errr);
-
-    if ($(this).attr('data-once')!='already_clicked' ){
-        //your code here
-
-var  checkBox = document.getElementById("checkBoxesInput"),
-  v = document.getElementById("button").value,
- el = document.getElementById("button");
-var x = t;
-console.log("1: X = ",x);
-if (x<=0){
-  console.log("2: X = ",x);
-$pop.show();
-}
-else {
-  console.log("1st hide");
-  $pop.hide();
-}
+$(document).ready(function(){
 
 
- $( "#button" ).click(function() {
-  console.log("button clicked");
-   x =1;
-
-   if (x=1){
-     console.log("2nd Hide");
-     console.log("3: X = ",x);
-     $pop.hide();
-
-
-   }
- });
-
-
-$("#popUp").css("margin-left", "0px");
-    $("#plus").css("margin-left", "-625px");
+var $pop = $('#popup');
 
 
     $("#close").click(function() {
@@ -54,36 +16,27 @@ $("#popUp").css("margin-left", "0px");
 	$(document).ready(function() {
   $("#notification-dropdown").hover(function(e) {
     var $btn = $('button', this);
-    if (!$btn.hasClass('clicked')) {
+      if (!$btn.hasClass('clicked')) {
+        var d = e.type
+      //  var g = $btn.hasClass
+        console.log("hover type= ",d);
+
       $btn.css("backgroundColor", e.type == 'mouseenter' ? "#fff" : '#9d9d9d');
-
-    }
-      }).click(function() {
-        $('button', this).addClass('clicked').css("backgroundColor", 'red');
-
+      }
+  }).click(function() {
+    //  $('button', this).addClass('clicked').css("backgroundColor", 'red');
         })
-
-
     });
 
 
-    }
-    else ;
+    //}
+    //else ;
 });
-$("#close").click(function() {
-  $("#popUp").css("margin-left", "-625px");
-  $("#plus").css("margin-left", "0px");
 
-});
+
 function myFunction6() {
 
       $pop = $('#popUp');
       $pop.show();
-
-};
-function myFunction4() {
-
-    $("#popUp").css("margin-left", "-625px");
-    $("#plus").css("margin-left", "0px");
 
 };
